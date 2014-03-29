dd086fa924f920894dd41556990cff8e53d6b93b
Rahul G. Krishnan <rahulk90@gmail.com>
2014-03-28 23:22:00 -0400

    wordnet

---
+++ python/wordnet/createHierarchy.py
@@ -0,0 +1,38 @@
+import shelve
+import numpy as np
+from nltk.corpus import wordnet as wn 
+
+
+#http://wordnet.princeton.edu/man/wngloss.7WN.html
+#http://www.nltk.org/howto/wordnet.html
+
+
+#create vectors and save in shelf
+def createVectors(wordlist):
+	db = shelve.open('wordnet.db',protocol=2)
+
+	for word in wordlist:
+		#create if word not in db
+		if word not in db:
+			data = wn.synsets(word,pos=wn.NOUN)
+			associated_words = []
+			for f in data:
+				associated_words += list(set([w for s in f.closure(lambda s:s.hyponyms()) for w in s.lemma_names]))
+			print word,":",",".join(associated_words)
+			db[w] = associated_words
+
+	db.close()
+	print "Done"
+
+def printVectors(wordlist):
+	for word in wordlist:
+		data = wn.synsets(word,pos=wn.NOUN)
+		associated_words = []
+		for f in data:
+			associated_words += list(set([w for s in f.closure(lambda s:s.hyponyms()) for w in s.lemma_names]))
+		print word,":",",".join(associated_words)
+	print "Done"
+
+if __name__ == '__main__':
+	wordlist = ['movies']
+	printVectors(wordlist)
