c872f6ac13214b995c32bcc72b3e006752207521
Rahul G. Krishnan <rahulk90@gmail.com>
2014-04-24 18:14:18 -0400

    cleaned up python dir

--- python/wordnet/analogicalReasoning.py
+++ python/wordnet/analogicalReasoning.py
@@ -3,15 +3,19 @@ import numpy as np
 DBLOC = '/misc/vlgscratch3/FergusGroup/rahul/vectors/GoogleNews.db'
 QUESTION_LOC = '../../word2vec/questions-words.txt'
 import os
-
-def testAnalogicalShelve():
+VEC_FILE = '/home/rahul/courses/mlcs/wordvectors/python/wordnet/answer-vec.vec'
+GRD_TRUTH = '/home/rahul/courses/mlcs/wordvectors/python/wordnet/answer-grdTruth.wd'
+RESULT_FILE = '/home/rahul/courses/mlcs/wordvectors/python/wordnet/results'
+NBRS  = 10
+from itertools import izip
+def setupShelve():
 	#Test on analogical reasoning from shelve database
 	db = shelve.open(DBLOC)
 	err_lines = []	
 	f_question = open(QUESTION_LOC,'r')
 
-	VEC_FILE = '/home/rahul/courses/mlcs/wordvectors/python/wordnet/answer-vec.vec'
-	GRD_TRUTH = '/home/rahul/courses/mlcs/wordvectors/python/wordnet/answer-grdTruth.wd'
+	
+	
 	f_answer   = open(VEC_FILE,'w')
 	f_truth    = open(GRD_TRUTH,'w')
 	can_test = float(0)
@@ -41,26 +45,28 @@ def testAnalogicalShelve():
 	f_truth.close()
 	print "For the Analogical Reasoning Text, we can test the accuracy on ",(can_test*100/total),"% of words"
 
+def getNearestWords():
 	print "Computing nearest words..."
 	BINFILE = '/misc/vlgscratch3/FergusGroup/rahul/vectors/GoogleNews-vectors-negative300.bin'
-	NBRS  = 10
-	RESULT_FILE = '/home/rahul/courses/mlcs/wordvectors/python/wordnet/results'
 	cmd = '../../word2vec/nearest_words '+BINFILE+' '+str(NBRS)+' '+VEC_FILE+' '+RESULT_FILE 
 	os.system(cmd)
 
-	print "Computing results:"
-
-	with open(RESULT_FILE) as t1, open(GRD_TRUTH) as t2: 
+def testSolutions():
+	print "Computing accuracy:"
+	acc_k = [0]*NBRS
+	n=0
+	with open(RESULT_FILE+'.wd') as t1, open(GRD_TRUTH) as t2: 
 		for results, truth in izip(t1, t2):
 			val = truth.strip()
 			elems = [e.strip() for e in results.strip().split('|')]
-			if val in elems:
-				found +=1
-			else:
-				notfound+=1
+			for k in range(1,NBRS):
+				if val in elems[:k]:
+					acc_k[k] +=1
 			n+=1
-	print "Test Accuracy: ",float(found)*100/n," %" 
+	for k in range(1,NBRS):
+		print "Test Accuracy: Top ",k," Accuracy= ",float(acc_k[k])*100/n," %" 
 
 if __name__ == "__main__":
-	testAnalogicalShelve()
+	#setupShelve()
+	testSolutions()
 # ./nearest_words /misc/vlgscratch3/FergusGroup/rahul/vectors/GoogleNews-vectors-negative300.bin 12 /home/rahul/courses/mlcs/wordvectors/python/wordnet/input_file /home/rahul/courses/mlcs/wordvectors/python/wordnet/output_file
