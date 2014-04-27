9e966a0326e96097cab13c1867397a737d6cb663
Rahul G. Krishnan <rahulk90@gmail.com>
2014-04-27 17:55:57 -0400

    added stuff in mat

--- matlab/getIndicesFromAnalogicalReasoning.m
+++ matlab/getIndicesFromAnalogicalReasoning.m
@@ -1,9 +1,49 @@
-function [ output_args ] = getIndicesFromAnalogicalReasoning( k,N_small )
-%return (N_small,1) vector of indices
+function [idx_final] = getIndicesFromAnalogicalReasoning(k,N_small)
 
-load('./generated_mats/analog_word_vectors.mat')
-load('./generated_mats/analog_word_indices.mat')
-load('./generated_mats/analog_word_words.mat')
+  load('./generated_mats/analog_question_indices.mat')
+
+  idx_final = zeros(N_small,1);
+  t = 1;
+
+  N_sec = 14;
+  loc{1}='./qwSections/questions-capital-common-countries.sec';
+  loc{2}='./qwSections/questions-capital-world.sec';
+  loc{3}='./qwSections/questions-city-in-state.sec';
+  loc{4}='./qwSections/questions-currency.sec';
+  loc{5}='./qwSections/questions-family.sec';
+  loc{6}='./qwSections/questions-gram1-adjective-to-adverb.sec';
+  loc{7}='./qwSections/questions-gram2-opposite.sec';
+  loc{8}='./qwSections/questions-gram3-comparative.sec';
+  loc{9}='./qwSections/questions-gram4-superlative.sec';
+  loc{10}='./qwSections/questions-gram5-present-participle.sec';
+  loc{11}='./qwSections/questions-gram6-nationality-adjective.sec';
+  loc{12}='./qwSections/questions-gram7-past-tense.sec';
+  loc{13}='./qwSections/questions-gram8-plural.sec';
+  loc{14}='./qwSections/questions-gram9-plural-verbs.sec';
+
+
+  %randomly pick k of the sections
+  sections_picked = randi(N_sec,k,1);
+  %specify sections to pick
+  sections_picked = [1;2;3];
+  k=3;
+
+  line_num = 1;
+  for i=1:14
+     doc = textscan(fopen(loc{i}),'%s %s %s %s'); 
+     
+     doc_size = max(size(doc{1}));
+     all_idx = analog_question_indices(line_num:line_num+doc_size-1,:);
+     indices_from_section = unique(all_idx(:));
+     fprintf('Size %d: %d Unique: %d\n',i,max(size(doc{1})),size(indices_from_section,1));
+     if sum(sections_picked==i)==1
+        idx_final(t:t+size(indices_from_section,1)-1) = indices_from_section;
+        t = t +size(indices_from_section,1);
+     end
+
+  end
+
+  idx_final(t:end) = randi(300000,size(idx_final(t:end)));
+
 
 end
-
--- matlab/project_diff.m
+++ matlab/project_diff.m
@@ -9,7 +9,7 @@ X = fread(fid, [N S], 'single');
 
 %Get interesting indices
 %(for now, randomly subsample some indices)
-N_small = 2000;
+N_small = 1000;
 
 %the number of principle components we expect to see
 k = 3;
@@ -20,10 +20,9 @@ k = 3;
 
 %It randomly samples vectors from up to k different sections in the
 %analogical reasoning test
-idx = getIndicesFromAnalogicalReasoning(k,N_small);
 
+idx_final = getIndicesFromAnalogicalReasoning(k,N_small);
 %idx = randi(N,N_small,1);
-
 
 
 %Generate all pairs
@@ -42,7 +41,16 @@ for i=1:N_small
         t = t +1;
         X_pairs(t,:) = X_sub(j,:)-X_sub(i,:);
         t = t+1;
+        %if t>size(X_pairs,1)
+        %	fprintf('Error. Out of bounds impending..t = %d',t);
+        %end
     end
 end
+X_pairs = X_pairs(1:t-1,:);
 
-[U, S, V] = svds(X_pairs,3);
+[U, S, V] = svds(X_pairs_t,3);
+
+
+
+
+
