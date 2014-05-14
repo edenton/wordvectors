27565163c12c08c8e21ce8a975d4840e9389552f
Rahul G. Krishnan <rahulkrishnan@128-122-47-233.dynapool.nyu.edu>
2014-05-14 11:38:17 -0400

    docs

--- python/analogy/genplot.py
+++ python/analogy/genplot.py
@@ -17,3 +17,12 @@ f.close()
 data = [() for (a,b,c) in izip(data_googlevec,data_cbow,data_skip_gram)]
 
 sections = [t.strip().split(' ')[1] for t in open('sectionsAnalogicalReasoning.txt').readlines()]
+
+
+plt.figure(1)
+plt.subplot(211)
+plt.plot(t1, f(t1), 'bo', t2, f(t2), 'k')
+
+plt.subplot(212)
+plt.plot(t2, np.cos(2*np.pi*t2), 'r--')
+plt.show()
